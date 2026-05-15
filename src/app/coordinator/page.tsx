adfda130121e6c88944168c234d5df82b8cfaaa4
Big Family / Samuel <thebigfamilyprogram@gmail.com>
2026-05-14 22:43:28 -0500

    Fix prerendering for Vercel deploy

--- src/app/coordinator/page.tsx
+++ src/app/coordinator/page.tsx
@@ -1,4 +1,6 @@
-﻿import { redirect } from 'next/navigation'
+﻿export const dynamic = 'force-dynamic'
+
+import { redirect } from 'next/navigation'
 import { createSupabaseServerClient, createSupabaseAdminClient } from '@/lib/supabase-server'
 import CoordinatorClient from './CoordinatorClient'
 
